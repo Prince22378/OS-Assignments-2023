a21ea7bc47c3078db257f264126d966fb7a25f7c
Prince22378 <121478109+Prince22378@users.noreply.github.com>
2023-09-11 00:12:18 +0530

    including header file

---
+++ Assignment-2/header.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <signal.h> 
+#include <fcntl.h>  
+#include <ctype.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+
